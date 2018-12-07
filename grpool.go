b04b1096901f3fafb546266f6951a539c83f0e46
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-12-07 22:05:58 +0900

    fix fatal error

--- grpool.go
+++ grpool.go
@@ -27,20 +27,17 @@ type grPool struct {
 
 // New --
 func New(opts ...Option) GrPool {
-	gr := new(grPool)
+	gr := &grPool{
+		size:       DefaultPoolSize,
+		runnableCh: make(chan runnable),
+	}
 
 	for _, opt := range opts {
 		opt(gr)
 	}
 
-	if gr.size < 1 {
-		gr.size = DefaultPoolSize
-	}
-
 	for i := 0; i < gr.size; i++ {
-		go func(runnableCh chan runnable) {
-			async(runnableCh)
-		}(gr.runnableCh)
+		go async(gr.runnableCh)
 	}
 
 	return gr
@@ -62,9 +59,7 @@ func (gp *grPool) Async(ctx context.Context, runner Runner) {
 }
 
 func async(runnableCh chan runnable) {
-	for runnable := range runnableCh {
-		runnable.runner(runnable.ctx)
+	for r := range runnableCh {
+		r.runner(r.ctx)
 	}
 }
-
-func (gp *grPool) Close() {}
