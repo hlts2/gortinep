61f9bd367df8720bb04514749d71b4590064bf79
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-12-20 23:57:58 +0900

    Added test case for chaininterceptor

--- middlewares/chain_test.go
+++ middlewares/chain_test.go
@@ -40,4 +40,31 @@ func TestChainInterceptors(t *testing.T) {
 		}
 
 	})
+
+	t.Run("n == 1", func(t *testing.T) {
+		var got string
+		chainInterceptor := ChainInterceptors(
+			func(ctx context.Context, job grpool.Job) error {
+				got += "enter interceptor1\n"
+				err := job(ctx)
+				got += "finish interceptor1\n"
+				return err
+			},
+		)
+
+		err := chainInterceptor(context.Background(), func(ctx context.Context) error {
+			got += "job\n"
+			return nil
+		})
+
+		if err != nil {
+			t.Errorf("chainInterceptor is err: %v", err)
+		}
+
+		expected := "enter interceptor1\njob\nfinish interceptor1\n"
+		if got != expected {
+			t.Errorf("chainInterceptor is wrong. expected %v, but got: %v", expected, got)
+		}
+
+	})
 }
