15533e2c387d294342cdc25b1c116b67dfb3c9e0
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-12-24 12:00:52 +0900

    Fix grpool to gorpool

--- middlewares/recovery/recovery_interceptor.go
+++ middlewares/recovery/recovery_interceptor.go
@@ -1,17 +1,17 @@
-package grpool_recovery
+package gorpool_recovery
 
 import (
 	"context"
 
-	"github.com/hlts2/grpool"
+	"github.com/hlts2/gorpool"
 )
 
 // RecoveryHandlerFunc is a function that recovers from the panic `p`.
 type RecoveryHandlerFunc func(p interface{})
 
 // Interceptor returns a new interceptor for panic recovery.
-func Interceptor(f RecoveryHandlerFunc) grpool.Interceptor {
-	return func(ctx context.Context, runner grpool.Job) error {
+func Interceptor(f RecoveryHandlerFunc) gorpool.Interceptor {
+	return func(ctx context.Context, runner gorpool.Job) error {
 		defer func() {
 			if p := recover(); p != nil {
 				f(p)
