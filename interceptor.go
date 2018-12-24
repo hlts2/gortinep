afdd0cc4ba9e8fc71bbb0d5aaba74aeeda02e826
hlts2 <hiroto.funakoshi.hiroto@gmail.com>
2018-12-24 12:34:40 +0900

    Fix typo

--- interceptor.go
+++ interceptor.go
@@ -2,7 +2,7 @@ package gortinep
 
 import "context"
 
-// Job defines the handler of job for goruotine pool.
+// Job defines the handler of job for goroutine pool.
 type Job func(context.Context) error
 
 // Interceptor provides a hook to intercept the execution of Job.
